fac93f2526610a29b648346c5f29b24d3435cd48
OmarAdelShalaan <omaradelshalaan@gmail.com>
2024-02-05 01:22:09 +0200

    0x03

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+        list = my_list
+        if idx < 0 or idx > len(list):
+                return list
+        else:
+                list[idx] = element
+                return list
