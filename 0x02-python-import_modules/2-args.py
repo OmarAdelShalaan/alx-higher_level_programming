11432a6d5fbeb82fb4d9b385f6075e58e43f85a5
OmarAdelShalaan <omaradelshalaan@gmail.com>
2024-02-07 00:29:59 +0200

    0x02

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,17 +1,12 @@
 #!/usr/bin/python3
-#!/usr/bin/python3
-def Print_Argv(list = []):
-        if len(list) == 0:
-                print("0 arguments.")
-        elif len(list) == 1:
-                print(f"{len(list)} argument:")
-                print(f"1: {list[0]:s}")
-        else:
-                print(f"{len(list)} arguments:")
-                for i in range(len(list)):
-                        print(f"{i + 1}: {list[i]}")
-                 
 if __name__ == "__main__":
     import sys
-    Print_Argv(sys.argv)
-    
+    count = len(sys.argv) - 1
+    if count == 1:
+        print("{:d} argument:".format(count))
+    elif count == 0:
+        print("{:d} arguments.".format(count))
+    else:
+        print("{:d} arguments:".format(count))
+    for i in range(1, count + 1):
+        print("{:d}: {}".format(i, sys.argv[i]))
